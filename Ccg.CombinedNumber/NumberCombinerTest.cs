97b4cd7878ca16402349bbde741e68e21278cf49
Michel <michelschep@gmail.com>
2020-03-11 13:39:04 +0100

    Add the tests

--- Ccg.CombinedNumber/NumberCombinerTest.cs
+++ Ccg.CombinedNumber/NumberCombinerTest.cs
@@ -2,17 +2,26 @@ using NUnit.Framework;
 
 namespace Ccg.CombinedNumber
 {
-    public class Tests
+    public class NumberCombinerTest
     {
+        private NumberCombiner _numberCombiner;
+
         [SetUp]
         public void Setup()
         {
+            _numberCombiner = new NumberCombiner();
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(new[] {50, 2, 1, 9}, "95021")]
+        [TestCase(new[] {5, 50, 56}, "56550")]
+        [TestCase(new[] {420, 42, 423}, "42423420")]
+        [TestCase(new[] {0, 0, 0}, "0")]
+        [TestCase(new[] {1, 1, 1}, "111")]
+        [TestCase(new[] {8, 50, 100}, "850100")]
+        [TestCase(new[] {2, 1, 4, 7, 4, 8, 3, 6, 4, 7}, "8776444321")]
+        public void TestNumberCombiner(int[] input, string expected)
         {
-            Assert.Pass();
+            Assert.Equals(_numberCombiner.Combine(input), expected);
         }
     }
 }
