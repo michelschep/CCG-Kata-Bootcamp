db909aead09adafe51bd24f757e55059f0a12512
Michel <michelschep@gmail.com>
2020-03-11 13:22:43 +0100

    Add unit test cases

--- Ccg.FizzBuzz/FizzBuzzMapperTest.cs
+++ Ccg.FizzBuzz/FizzBuzzMapperTest.cs
@@ -16,11 +16,14 @@ namespace Ccg.FizzBuzz
         [TestCase(1, "1")]
         [TestCase(2, "2")]
         [TestCase(3, "Fizz")]
+        [TestCase(13, "13")]
         [TestCase(99, "Fizz")]
         [TestCase(5, "Buzz")]
+        [TestCase(51, "51")]
         [TestCase(500, "Buzz")]
         [TestCase(15, "FizzBuzz")]
         [TestCase(300, "FizzBuzz")]
+        [TestCase(351, "351")]
         public void TestFizzBuzz(int input, string expected)
         {
             Assert.Equals(_fizzBuzzMapper.Map(input), expected);
