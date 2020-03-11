836817de60ba473d3fa3d0161dd91e95af7e3558
Michel <michelschep@gmail.com>
2020-03-11 13:34:49 +0100

    Add combine number test

---
+++ Ccg.CombinedNumber/NumberCombiner.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Ccg.CombinedNumber
+{
+    public class NumberCombiner
+    {
+        public string Combine(int[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ccg.CombinedNumber/NumberCombinerTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Ccg.CombinedNumber
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
